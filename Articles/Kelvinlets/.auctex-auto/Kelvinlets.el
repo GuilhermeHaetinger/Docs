bc5d3817ef95af3b7e0b5cae75e292c705e85fdb
Guilherme G. Haetinger <ghaetinger@gmail.com>
2020-06-09 10:56:04 -0700

    Added Kelvinlets base

---
+++ Articles/Kelvinlets/.auctex-auto/Kelvinlets.el
@@ -0,0 +1,48 @@
+(TeX-add-style-hook
+ "Kelvinlets"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("IEEEtran" "10pt" "conference")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "IEEEtran"
+    "IEEEtran10"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "color"
+    "listings"
+    "authblk")
+   (LaTeX-add-labels
+    "sec:orgecedf30"
+    "sec:org7e934c1"
+    "sec:orge3a53ee"
+    "sec:org330ed76"
+    "sec:org6f4538e"
+    "sec:org127d858")
+   (LaTeX-add-bibliographies))
+ :latex)
+
